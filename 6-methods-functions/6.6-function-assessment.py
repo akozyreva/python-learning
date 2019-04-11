8434ce88cfb8e271d774b155232bf0f6ed510023
akozyreva <anna.ko4523546@gmail.com>
2019-04-11 15:12:39 +0200

    add more tasks

--- 6-methods-functions/6.6-function-assessment.py
+++ 6-methods-functions/6.6-function-assessment.py
@@ -27,3 +27,34 @@ def makes_twenty(n1,n2):
 
 print(makes_twenty(20,10))
 print(makes_twenty(2,3))
+
+# OLD MACDONALD: Write a function that capitalizes
+# the first and fourth letters of a name
+def old_macdonald(name):
+    str_list = list(name)
+    str_list[3] = str_list[3].upper()
+    str_list[0] = str_list[0].upper()
+    name = "".join(str_list)
+    return name
+print(old_macdonald('macdonald'))
+
+#  MASTER YODA: Given a sentence, return a
+#  sentence with the words reversed
+def master_yoda(text):
+    text = text.split()[::-1]
+    text = " ".join(text)
+    return text
+print(master_yoda('I am home'))
+print(master_yoda('We are ready'))
+
+#  ALMOST THERE: Given an integer n, return
+# True if n is within 10 of either 100 or 200
+def almost_there(num):
+    if (abs(100 - num) <= 10) or (abs(200 - num) <= 10):
+        return True
+    else:
+        return False
+print(almost_there(90))
+print(almost_there(104))
+print(almost_there(150))
+print(almost_there(209))
