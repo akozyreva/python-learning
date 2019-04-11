159dcc6b351d532501771aebf6b7ef767f7d4737
akozyreva <anna.ko4523546@gmail.com>
2019-04-11 17:51:11 +0200

    add summer_69 function

--- 6-methods-functions/6.6.1-function-assessment.py
+++ 6-methods-functions/6.6.1-function-assessment.py
@@ -31,3 +31,31 @@ def blackJack(a, b, c):
 print(blackJack(5,6,7))
 print(blackJack(9,9,9))
 print(blackJack(9,9,11))
+
+
+# SUMMER OF '69: Return the sum of the numbers in the array,
+# except ignore sections of numbers starting with a 6 and extending
+# to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+# summer_69([1, 3, 5]) --> 9
+# summer_69([4, 5, 6, 7, 8, 9]) --> 9
+# summer_69([2, 1, 6, 9, 11]) --> 14
+
+print("summer_69")
+def summer_69(arr):
+    sum = 0
+    for index, val in enumerate(arr):
+        if val != 6:
+            sum += val
+        else:
+            for new_val in arr[index + 1: ]:
+                if new_val != 9:
+                    arr.remove(new_val)
+                else:
+                    arr.remove(new_val)
+                    break
+        print(arr)
+    return sum
+
+print(summer_69([1, 3, 5]))
+print(summer_69([4, 5, 6, 7, 8, 9]))
+print(summer_69([2, 1, 6, 9, 11]))
