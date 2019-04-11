eb054754fc0c71ac0354108fb0f1bf1187ad0d4d
akozyreva <anna.ko4523546@gmail.com>
2019-04-11 12:19:00 +0200

    add one more task

--- 6-methods-functions/6.6-function-assessment.py
+++ 6-methods-functions/6.6-function-assessment.py
@@ -17,3 +17,13 @@ def animal_crackers(text):
 
 print(animal_crackers('Levelheaded Llama'))
 print(animal_crackers('Crazy Kangaroo'))
+
+# MAKES TWENTY: Given two integers, return True if the sum of the integers
+# is 20 or if one of the integers is 20. If not, return False
+def makes_twenty(n1,n2):
+    if (n1 == 20) or (n2 == 20) or ((n1 + n2) == 20):
+        return True
+    return False
+
+print(makes_twenty(20,10))
+print(makes_twenty(2,3))
