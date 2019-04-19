ff560e91346b5086b9667d69d06992268f589b50
Ваше Имя <anna.ko4523546@gmail.com>
2019-04-19 10:28:46 +0200

    add example with Cylinder class

--- 8-obj-oriented-programming/8.5-home-work.py
+++ 8-obj-oriented-programming/8.5-home-work.py
@@ -24,3 +24,24 @@ coordinate2 = (8, 10)
 li = Line(coordinate1, coordinate2)
 print(li.distance())
 print(li.slope())
+
+
+class Cylinder:
+
+    pi = 3.14
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        return Cylinder.pi * self.height * self.radius**2
+
+    def surface_area(self):
+        return (2 * Cylinder.pi * self.radius**2)\
+                + (2 * Cylinder.pi * self.radius * self.height)
+
+
+c = Cylinder(2, 3)
+print(c.volume())
+print(c.surface_area())
