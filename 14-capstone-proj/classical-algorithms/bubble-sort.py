166b0368c6f3cd9f31eb1d5adc6d01dbe4e49cac
akozyreva <anna.ko4523546@gmail.com>
2019-04-25 11:40:38 +0200

    add more logs

--- 14-capstone-proj/classical-algorithms/bubble-sort.py
+++ 14-capstone-proj/classical-algorithms/bubble-sort.py
@@ -13,13 +13,22 @@ alist = list(create_random_list(2, 7))
 
 def bubbleSort(alist):
     for passnum in range(len(alist)-1, 0, -1):
+        print("Begin of iteration")
+        print("================")
+        print("iteration, when sorts elements =", passnum + 1)
         for i in range(passnum):
+            print("check values", alist[i], alist[i+1])
             if alist[i] > alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
+                print("new values!", alist[i], alist[i + 1])
+            print(alist)
+        print("End of iteration")
+        print("==========")
 
 
-print(alist)
+
+print('initial list', alist)
 bubbleSort(alist)
-print(alist)
+print('final list', alist)
